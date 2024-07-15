d0807d2716628ce44908cbff09e522bb605ccd10
brunonascimento-dev <bn.id07@gmail.com>
2024-07-15 19:41:47 -0300

    ultima pratica desse bloco

--- trilha-basica-java/java-anatomia/src/MinhaClasse.java
+++ trilha-basica-java/java-anatomia/src/MinhaClasse.java
@@ -1,5 +1,16 @@
 public class MinhaClasse {
     public static void main (String [] args) {
+
+        final String BR = "Brasil";
+        double PI = 3.14;
+        int ESTADOS_BRASILEIROS = 27;
+        int ANO_2000 = 2000;
+
+        System.out.println(ESTADOS_BRASILEIROS);
+        System.out.println(ANO_2000);
+        System.out.println(BR);
+        System.out.println(PI);
+
         System.out.print("Boas vindas ao início do aprendizado de java!");
     }
 }
