b464ca08d5962c7f2e0d8ecda79cc7b03c22142d
brunonascimento-dev <bn.id07@gmail.com>
2024-07-16 13:03:16 -0300

    métodos

--- trilha-basica-java/java-anatomia-classes/java-anatomia-classes/src/MinhaClasse.java
+++ trilha-basica-java/java-anatomia-classes/java-anatomia-classes/src/MinhaClasse.java
@@ -1,7 +1,23 @@
 public class MinhaClasse {
     public static void main (String [] args) {
         String meuNome = "Bruno";
+        int anoFabricacao = 2024;
+        boolean verdadeira = true;
+
+        String primeiroNome = "Bruno";
+        String segundoNome = "Nascimento";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+        System.out.println(nomeCompleto);
+
 
         System.out.println(meuNome);
+        System.out.println(anoFabricacao);
+        System.out.println(verdadeira);
+
+    }
+
+    public static String nomeCompleto (String primeiroNome, String segundoNome) {
+        return primeiroNome.concat(" ").concat(segundoNome);
     }
 }
